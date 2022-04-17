52f928a872c58e833f9979c9ad712527ecc76960
Vojtěch Zikmund <jsem@vzikmund.cz>
2022-04-17 11:41:17 +0200

    Routing

--- public/index.php
+++ public/index.php
@@ -2,4 +2,22 @@
 
 require_once "../vendor/autoload.php";
 
-echo "hello world";
+Flight::route("GET /", function(){
+    echo "hello from empty method";
+});
+
+Flight::route("GET|POST /first_route", function(){
+    echo "hello from the first route";
+});
+
+Flight::route("POST /post_method", function(){
+    echo "hello from post";
+});
+
+# POST
+# GET
+# PUT
+# DELETE
+# PATCH
+
+Flight::start();
